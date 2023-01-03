64e7157bd1d6f02d15bf7d2778a799a0425baee9
EL GHAZY MOHAMED REDA <92031932+ELghazymohamedreda@users.noreply.github.com>
2023-01-03 10:28:31 +0100

    update

--- Fichier.js
+++ Fichier.js
@@ -1,83 +1,77 @@
 var DATA = new XMLHttpRequest();
-  DATA.open("GET", "Data.json");
-  DATA.onreadystatechange = function () {
-      var datalist = JSON.parse(DATA.responseText);
+DATA.open("GET", "Data.json");
+DATA.onreadystatechange = function () {
+  var datalist = JSON.parse(DATA.responseText);
+  afficher(datalist)
+
+
+  // RECHERCHE //
+
+  document.getElementById('searche').onkeyup = function () {
+
+    var searche = document.getElementById("searche").value;
+    if (searche == "") {
       afficher(datalist)
+    }
+    else {
+      for (var r = 0; r < searche.length; r++) {
+        var CALC = datalist.filter(function (item) {
 
+          return item.titre.toLowerCase()[r] == searche.toLowerCase()[r];
 
-      // RECHERCHE //
-
-      document.getElementById('searche').onkeyup=function(){
-        
-        var searche = document.getElementById("searche").value;
-        if(searche==""){
-          afficher(datalist)
-        }
-        else{
-          for (var r =0 ;r<searche.length;r++){
-               var CALC = datalist.filter(function(item){
-                
-              return item.titre.toLowerCase()[r]== searche.toLowerCase()[r];
-              
-            })
-          }
-          
-        }
-        
-        afficher(CALC)
+        })
       }
-      ////////////////////////////////////
-      // items.sort((a, b) => a.value - b.value); Numéros des liens de chat
-      document.getElementById('floatingSelect').onchange=function(){
-      var  COD = document.getElementById('floatingSelect').value;
-        if (COD=="1") {
-        datalist.sort((a, b) => a.titre.localeCompare(b.titre));
-        afficher(datalist)
-        }
-
-        else if (COD=="2"){
-        datalist.sort((a, b) => a.réalisateur.localeCompare(b.réalisateur));
-        afficher(datalist)
-      }
-
-      else if (COD=="3"){
-        datalist.sort((a,b)=> a.durée - b.durée) 
-        afficher(datalist)
-      }
-      else if(COD== "4"){
-        datalist.sort((a,b)=> b.durée - a.durée) 
-        afficher(datalist)
-      }
-
-      else if (COD=="5"){
-        datalist.sort((a,b)=> b.production - a.production) 
-        afficher(datalist)
-      }
-
-      else if (COD=="6"){
-        datalist.sort((a,b)=> a.production - b.production) 
-        afficher(datalist)
-      }
-
-
 
     }
 
-    
+    afficher(CALC)
+  }
+  ////////////////////////////////////
+  // items.sort((a, b) => a.value - b.value); Numéros des liens de chat
+  document.getElementById('floatingSelect').onchange = function () {
+    var COD = document.getElementById('floatingSelect').value;
+    if (COD == "1") {
+      datalist.sort((a, b) => a.titre.localeCompare(b.titre));
+      afficher(datalist)
+    }
+
+    else if (COD == "2") {
+      datalist.sort((a, b) => a.réalisateur.localeCompare(b.réalisateur));
+      afficher(datalist)
+    }
+
+    else if (COD == "3") {
+      datalist.sort((a, b) => a.durée - b.durée)
+      afficher(datalist)
+    }
+    else if (COD == "4") {
+      datalist.sort((a, b) => b.durée - a.durée)
+      afficher(datalist)
+    }
+
+    else if (COD == "5") {
+      datalist.sort((a, b) => b.production - a.production)
+      afficher(datalist)
+    }
+
+    else if (COD == "6") {
+      datalist.sort((a, b) => a.production - b.production)
+      afficher(datalist)
+    }
 
 
-    };
+
+  }
+
+};
+
+//////////////////////////////////////////////////////////////////
 
 
-
-
-      //////////////////////////////////////////////////////////////////
-
-    
-function afficher(datalist){
-      var table = "";
-      for (let i = 0; i < datalist.length; i++) {
-        table += `
+function afficher(datalist) {
+  var table = "";
+  for (let i = 0; i < datalist.length; i++) {
+    table += `
                       <tr>
                         <td>${datalist[i].titre}</td>
                         <td>${datalist[i].réalisateur}</td>
@@ -88,7 +82,7 @@ function afficher(datalist){
                         <td>${datalist[i].Acteurs[0].sonmon} ${datalist[i].Acteurs[0].somprenom} ${datalist[i].Acteurs[0].sanationalité}</td>
                       </tr>
         `;
-      }
-    document.getElementById("tbody").innerHTML = table;
-    }
-  DATA.send();
+  }
+  document.getElementById("tbody").innerHTML = table;
+}
+DATA.send();
